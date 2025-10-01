ce58f29f3485ee6b2d8f3d416288d00dbc2ff0cc
Iqbal Syahbana <iqbalsyahbana14@gmail.com>
2025-10-01 14:56:06 +0700

    Add authentication routes and JWT login functionality

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const saldoRoutes = require("./saldoRoutes");
 const userRoutes = require("./userRoutes");
-
+const authRoutes = require("./authRoutes")
 const router = express.Router();
 
 // Register semua routes
+router.use("/auth", authRoutes);
 router.use("/saldo", saldoRoutes);
 router.use("/users", userRoutes);
 
